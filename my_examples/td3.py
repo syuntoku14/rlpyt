d1d2de7afbc47255654330b00201fdc97baeff5c
Toshinori Kitamura <syuntoku14@gmail.com>
2020-02-26 21:14:21 +0900

    Add my_examples

---
+++ my_examples/td3.py
@@ -0,0 +1,85 @@
+
+"""
+Runs multiple instances of the Atari environment and optimizes using A2C
+algorithm. Can choose between configurations for use of CPU/GPU for sampling
+(serial or parallel) and optimization (serial).
+
+Alternating sampler is another option.  For recurrent agents, a different mixin
+is required for alternating sampling (see rlpyt.agents.base.py), feedforward agents
+remain unaffected.
+
+"""
+from rlpyt.samplers.serial.sampler import SerialSampler
+from rlpyt.samplers.parallel.cpu.sampler import CpuSampler
+from rlpyt.samplers.parallel.gpu.sampler import GpuSampler
+from rlpyt.samplers.parallel.gpu.alternating_sampler import AlternatingSampler
+from rlpyt.envs.gym import make as gym_make
+from rlpyt.algos.qpg.td3 import TD3
+from rlpyt.agents.qpg.td3_agent import Td3Agent
+from rlpyt.runners.minibatch_rl import MinibatchRl
+from rlpyt.utils.logging.context import logger_context
+
+
+def build_and_train(env_id="LunarLandarContinuous-v2", run_ID=0, cuda_idx=None, sample_mode="serial", n_parallel=2):
+    affinity = dict(cuda_idx=cuda_idx, workers_cpus=list(range(n_parallel)))
+    gpu_cpu = "CPU" if cuda_idx is None else f"GPU {cuda_idx}"
+    if sample_mode == "serial":
+        Sampler = SerialSampler  # (Ignores workers_cpus.)
+        print(f"Using serial sampler, {gpu_cpu} for sampling and optimizing.")
+    elif sample_mode == "cpu":
+        Sampler = CpuSampler
+        print(f"Using CPU parallel sampler (agent in workers), {gpu_cpu} for optimizing.")
+    elif sample_mode == "gpu":
+        Sampler = GpuSampler
+        print(f"Using GPU parallel sampler (agent in master), {gpu_cpu} for sampling and optimizing.")
+    elif sample_mode == "alternating":
+        Sampler = AlternatingSampler
+        affinity["workers_cpus"] += affinity["workers_cpus"]  # (Double list)
+        affinity["alternating"] = True  # Sampler will check for this.
+        print(f"Using Alternating GPU parallel sampler, {gpu_cpu} for sampling and optimizing.")
+
+    sampler = Sampler(
+        EnvCls=gym_make,
+        env_kwargs=dict(id=env_id),
+        eval_env_kwargs=dict(id=env_id),
+        batch_T=5,  # One time-step per sampler iteration.
+        batch_B=16,  # One environment (i.e. sampler Batch dimension).
+        max_decorrelation_steps=0,
+        eval_n_envs=10,
+        eval_max_steps=int(51e3),
+        eval_max_trajectories=50,
+    )
+    algo = TD3()  # Run with defaults.
+    agent = Td3Agent()
+    runner = MinibatchRl(
+        algo=algo,
+        agent=agent,
+        sampler=sampler,
+        n_steps=1e6,
+        log_interval_steps=1e4,
+        affinity=affinity,
+    )
+    config = dict(env_id=env_id)
+    name = "td3" + env_id
+    log_dir = "td3"
+    with logger_context(log_dir, run_ID, name, config):
+        runner.train()
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--env_id', type=str, help='environment ID', default='LunarLanderContinuous-v2')
+    parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
+    parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
+    parser.add_argument('--sample_mode', help='serial or parallel sampling',
+        type=str, default='cpu', choices=['serial', 'cpu', 'gpu', 'alternating'])
+    parser.add_argument('--n_parallel', help='number of sampler workers', type=int, default=4)
+    args = parser.parse_args()
+    build_and_train(
+        env_id=args.env_id,
+        run_ID=args.run_ID,
+        cuda_idx=args.cuda_idx,
+        sample_mode=args.sample_mode,
+        n_parallel=args.n_parallel,
+    )
